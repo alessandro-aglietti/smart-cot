5243ffbf16b314ba26947b1ea3819d663e93f545
cwz.scardigno@gmail.com <TiAmoRaff@47>
2015-04-11 23:45:08 +0200

    no message

--- Smart-cot/app/src/main/java/com/croccio/smart_cot/MainActivity.java
+++ Smart-cot/app/src/main/java/com/croccio/smart_cot/MainActivity.java
@@ -3,11 +3,14 @@ package com.croccio.smart_cot;
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.VideoView;
 
 import com.croccio.snap_mic.SnapMic;
 import com.croccio.snap_mic.callback.SnapCallback;
 import com.croccio.snap_mic.exception.VoiceRecorderAlreadyStartedException;
 import com.croccio.snap_mic.exception.VoiceRecorderIsNull;
+import com.croccio.video.PlayVideo;
 
 public class MainActivity extends Activity implements SnapCallback {
 
@@ -30,5 +33,13 @@ public class MainActivity extends Activity implements SnapCallback {
     @Override
     public void onMaxAmplitudeChanged(int amplitude) {
         Log.d(getClass().getName() + "onMaxAmplitude", "amplitude = " + amplitude);
+
+        if (findViewById(R.id.videoView).getVisibility() == View.GONE && amplitude >= 4000) {
+            PlayVideo.play(this, (VideoView) findViewById(R.id.videoView), R.raw.ninna_nanna);
+            findViewById(R.id.videoView).setVisibility(View.VISIBLE);
+        } else if (amplitude < 4000) {
+            ((VideoView) findViewById(R.id.videoView)).stopPlayback();
+            findViewById(R.id.videoView).setVisibility(View.GONE);
+        }
     }
 }
---
+++ Smart-cot/video/src/main/java/com/croccio/video/PlayVideo.java
@@ -0,0 +1,62 @@
+package com.croccio.video;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.Log;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+/**
+ * Created by antonioscardigno on 11/04/15.
+ */
+public class PlayVideo {
+
+    private static VideoView myVideoView;
+    private static int position = 0;
+    private static ProgressDialog progressDialog;
+    private static MediaController mediaControls;
+
+    public static void play(Context context, VideoView videoView, int video) {
+        myVideoView = videoView;
+
+        if (mediaControls == null) {
+            mediaControls = new MediaController(context);
+        }
+
+        progressDialog = new ProgressDialog(context);
+        progressDialog.setMessage("Caricamento...");
+        progressDialog.setCancelable(false);
+        //progressDialog.show();
+
+        try {
+            myVideoView.setMediaController(mediaControls);
+
+            myVideoView.setVideoURI(Uri.parse("android.resource://" + context.getPackageName() + "/" + video));
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        myVideoView.requestFocus();
+        //we also set an setOnPreparedListener in order to know when the video file is ready for playback
+        myVideoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+
+            public void onPrepared(MediaPlayer mediaPlayer) {
+                // close the progress bar and play the video
+                //progressDialog.dismiss();
+                //if we have a position on savedInstanceState, the video playback should start from here
+                myVideoView.seekTo(position);
+                if (position == 0) {
+                    myVideoView.start();
+                } else {
+                    //if we come from a resumed activity, video playback will be paused
+                    myVideoView.pause();
+                }
+            }
+        });
+    }
+
+}
